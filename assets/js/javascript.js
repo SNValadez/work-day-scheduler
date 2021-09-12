4064773994268656c136b6404b1237c4f653252e
SNValadez <snvaladez13@gmail.com>
2021-09-12 12:56:24 -0700

    time tracking

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,7 @@
-console.log("Hello");
-$(document).ready(function() {
-    console.log($(".saveBtn"))
-})
+// console.log("Hello");
+// $(document).ready(function() {
+//     console.log($(".saveBtn"))
+// })
 
 var currentDay = document.getElementById("currentDay")
 moment().day(7|currentDay);
@@ -15,3 +15,33 @@ var currentDay2 = moment(day).format("dddd, MMMM Do YYYY, h:mm:ss a");
 console.log (moment(day).format("dddd, MMMM Do YYYY, h:mm:ss a"))
 
 $(currentDay).append(currentDay2);
+
+//var events = JSON.parse(localStorage.getItem("saved events"));
+
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+
+    })
+
+    function timeTracker () {
+        var timeNow = moment().hour();
+
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("present");
+                
+            }
+        }
+        
+        )
+    }
+}
